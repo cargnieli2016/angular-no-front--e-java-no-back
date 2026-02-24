3592f9fed26b16b4a70452724f798541279d0b42
Cargnieli-2016 <cargnieli2016@gmail.com>
2026-02-24 14:43:57 -0300

    ciclo de vida, interpolacao, event e property binding

--- projeto1/src/app/componentes/conteudo/conteudo.component.ts
+++ projeto1/src/app/componentes/conteudo/conteudo.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-conteudo',
-  templateUrl: './conteudo.component.html',
-  styleUrls: ['./conteudo.component.css']
+    selector: 'app-conteudo',
+    templateUrl: './conteudo.component.html',
+    styleUrls: ['./conteudo.component.css']
 })
-export class ConteudoComponent {
+export class ConteudoComponent implements OnInit {
+
+    public mensagem: string;
+    public valor: number;
+    public link_foto: string;
+
+    public constructor() {
+        this.mensagem = 'Eu sou o conteudo.component!';
+        this.valor = 0;
+        this.link_foto = '/assets/imagem2.jpg';
+    }
+
+    ngOnInit(): void {
+
+    }
+
+    public mudarValor() {
+        this.valor++;
+        this.link_foto = '/assets/download.jpg';
+        
+    }
 
 }
