d4f43482a6d4abd0c302121dbc3d6c80cabb39c7
Cargnieli-2016 <cargnieli2016@gmail.com>
2026-03-02 08:12:36 -0300

    Java

---
+++ Java/Continhas.java
@@ -0,0 +1,34 @@
+
+public class Continhas {
+
+    public static void main(String args[]) {
+        int a, b, c;
+        float d, e;
+        double f;
+
+        a = 27;
+        b = 4;
+        c = a + b;
+
+        System.out.println("Valor de c = " + c);
+
+        c = a - b;
+        System.out.println("Valor de c = " + c);
+
+        c = a * b;
+        System.out.println("Valor de c = " + c);
+
+        c = a / b;
+        System.out.println("Valor de c = " + c);
+
+        c = a % b;
+        System.out.println("O resto da divisão de c = " + c);
+
+        d = (float) 1.0;
+        System.out.println("Valor de d = " + d);
+
+        d = 1.0f / 2;
+        System.out.println("Valor de d = " + d);
+
+    }
+}
